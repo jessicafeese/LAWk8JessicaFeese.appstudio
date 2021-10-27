5f70652c1e94eaf2f81c01b8c604dcd2f6427317
jessicafeese <jaf32246@creighton.edu>
2021-10-27 16:21:27 -0500

    Program 3

--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -2,9 +2,26 @@ let exercises = ["pullups","pushups","situps","jogging","plank","curls"]
 let coreExercises = ["situps","plank"]
 
 favExercises.onshow=function(){
-
+  selExercises.clear()
+  for (i = 0; i < exercises.length; i++)
+    selExercises.addItem(exercises[i])
+  for (i = 0; i < coreExercises.length; i++)
+    selExercises.addItem(coreExercises[i])
 }
 
 
 
 
+
+btnSubmit.onclick=function(){
+  let message = "Your favorite exercises are: "
+  for (i = 0; i < selExercises.text.length; i++)
+     message = message + selExercises.text[i] + ", "
+  message = message.slice(0, -2)
+  lblBest.value = (message)
+}
+
+
+btnNext3.onclick=function(){
+  ChangeForm(mobileNav)
+}
