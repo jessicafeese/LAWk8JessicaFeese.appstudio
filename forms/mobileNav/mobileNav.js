e37af222fceb02132c672cf6fe6581449f17af3c
jessicafeese <jaf32246@creighton.edu>
2021-10-27 16:40:30 -0500

    Program 4

--- forms/mobileNav/mobileNav.js
+++ forms/mobileNav/mobileNav.js
@@ -4,4 +4,22 @@ hmbMenu.onclick=function(s){
   /* add a switch here to take the user to the form
      they chose. 
   */
-}
+  if(typeof(s) == "object") {
+    return
+  }else{
+    switch (s){
+      case"Dessert Voting":
+      ChangeForm(dessertVoting)
+      break
+      case"Describe You":
+      ChangeForm(describeYou)
+      break
+      case"Favorite Exercises":
+      ChangeForm(favExercises)
+      break
+      case"Mobile Nav":
+      ChangeForm(mobileNav)
+      break
+    }
+  }
+}       
