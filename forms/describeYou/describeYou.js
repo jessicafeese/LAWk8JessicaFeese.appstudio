800775ef8d01131f32219989f0dd04f9cac45d1a
jessicafeese <jaf32246@creighton.edu>
2021-10-27 16:06:55 -0500

    Program 1 & 2

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -1,5 +1,9 @@
 
+rdoDescribe.onchange=function(){
+   let userChoice = $("input[name=rdoDescribe]:checked").prop("value")
+   lblDescribe.value = (`I would agree that you are a ${userChoice} person too!`)
+}
 
-
-
-
+btnNext2.onclick=function(){
+  ChangeForm(favExercises)
+}
